9edcb5aa79a172a22a1419a302d97b5c0793fbe9
Venu Gopal A <venugopala@GMG.LOCAL>
2024-05-24 12:24:09 +0530

    added google form testcase

--- src/main/java/demo/TestCases.java
+++ src/main/java/demo/TestCases.java
@@ -11,11 +11,9 @@ import java.util.List;
 import java.util.Set;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
